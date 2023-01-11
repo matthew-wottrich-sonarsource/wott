ac56b0f3d6ac562fe4c61b6ce6d5463cd84230d5
matthew-wottrich-sonarsource <matthew.wottrich@sonarsource.com>
2023-01-11 10:18:54 +0100

    2

---
+++ testing.py
@@ -0,0 +1,8 @@
+sonartest=[1,2,3]
+sonarquality=[a,b,c]
+sonarcheck=[item1,item2,item3]
+sonarnotfound=[test,tester,testing]
+print(sonartest)
+print(sonarquality)
+print(sonarcheck)
+print(sonarnotfound)
